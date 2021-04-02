3a8968e2788d34ff04727eb8cff68886ac8733fc
Roberto Gomes <robertohgm@hotmail.com>
2021-04-02 13:22:17 -0300

    Implementação de migration

---
+++ src/TodoUsers.FluentMigrations/DbMigrations/Migrations/20210402131201_Create_User_Table.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace TodoUsers.FluentMigrations.DbMigrations.Migrations
+{
+    [Migration(20210402131201)]
+    public class _20210402131201_Create_User_Table : Migration
+    {
+        public override void Down() { }
+
+        public override void Up()
+        {
+            Create.Table("User")
+                .WithColumn("Id").AsString().PrimaryKey()
+                .WithColumn("FirstName").AsString(60).Nullable()
+                .WithColumn("LastName").AsString(60).Nullable()
+                .WithColumn("UserName").AsString(20).Nullable()
+                .WithColumn("Password").AsString(32).Nullable()
+                .WithColumn("Email").AsString(160).Nullable();
+        }
+    }
+}
---
+++ src/TodoUsers.FluentMigrations/DbMigrations/TableVersionMigration.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator.Runner.VersionTableInfo;
+
+namespace TodoUsers.FluentMigrations.DbMigrations
+{
+    [VersionTableMetaData]
+    public class TableVersionMigration : IVersionTableMetaData
+    {
+        public object ApplicationContext { get; set; }
+
+        public bool OwnsSchema => true;
+
+        public string SchemaName => "";
+
+        public string TableName => "TDU_Version";
+
+        public string ColumnName => "Version";
+
+        public string DescriptionColumnName => "Description";
+
+        public string UniqueIndexName => "TDU_IDX_Version";
+
+        public string AppliedOnColumnName => "AppliedOn";
+    }
+}
